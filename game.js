335676df01b41c14e26c4fc4723d105ae91d589a
Chillen <josh.jenkin@live.com>
2018-11-09 16:38:27 -0700

    coded html, js, css, complete

---
+++ game.js
@@ -0,0 +1,48 @@
+var wins = 0;
+var losses = 0;
+var guessLeft = 10;
+var computerChoice = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+
+var winsText = document.getElementById("wins-text");
+var lossText = document.getElementById("losses-text");
+var choicesLeft = document.getElementById("guess-left");
+var lettersGuessed = document.getElementById("lets-guessed");
+
+winsText.textContent = "Wins: " + wins;
+lossText.textContent = "Losses: " + losses;
+choicesLeft.textContent = "Guesses Left: " + guessLeft;
+lettersGuessed.textContent = "Your Guesses so far: ";
+
+function resetFunction() {
+    guessLeft = 10;
+    lettersGuessed.textContent = "Your Guesses so far: "
+}
+
+document.onkeyup = function(event) {
+    var userGuess = event.key;
+
+
+    var computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+
+        if (userGuess === computerGuess) {
+            wins++;
+            resetFunction();
+        }
+        if (userGuess != computerGuess) {
+            guessLeft--;
+        }
+        if (guessLeft <= 0) {
+            losses++;
+            resetFunction();
+        }
+        
+        winsText.textContent = "Wins: " + wins;
+        lossText.textContent = "Losses: " + losses;
+        choicesLeft.textContent = "Guesses Left: " + guessLeft;
+        var textnode = document.createTextNode(" " + userGuess + " ");
+        lettersGuessed.appendChild(textnode);
+
+
+    console.log(computerGuess);
+}
